502db406f41c0b5b7b6a77df1e712e5ff3725f04
Dhana Gadupooti <dhanag@ktechproduts.com>
2021-06-07 18:23:09 +0530

    change text coloraturas

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         myLabel.text = "welcome"
+        myLabel.textColor  = .green
     }
 
 
