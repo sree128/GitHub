5b26dd9813595b9f6a339c918803ec3d7625df3c
Dhana Gadupooti <dhanag@ktechproduts.com>
2021-06-07 18:07:11 +0530

    first commit

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myLabel.text = "welcome"
     }
 
 
